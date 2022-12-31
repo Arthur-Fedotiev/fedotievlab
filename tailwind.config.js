4dfed87008b29b9dc7a21ba6806d275fd7bd38d6
arturfedotiev <artur.fedotiew@gmail.com>
2022-12-31 16:48:07 +0100

    build: add font; script for prod deploy; adjust configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+// @ts-ignore
+const disabledCss = {
+  "code::before": false,
+  "code::after": false,
+};
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      typography: {
+        DEFAULT: { css: disabledCss },
+        sm: { css: disabledCss },
+        lg: { css: disabledCss },
+        xl: { css: disabledCss },
+        "2xl": { css: disabledCss },
+        "3xl": { css: disabledCss },
+        "4xl": { css: disabledCss },
+        "5xl": { css: disabledCss },
+      },
       flex: {
         0: "0 1 0%",
       },
