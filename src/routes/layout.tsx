546ab1adbddcf16d089ac0b807ab9cc26360324f
arturfedotiev <artur.fedotiew@gmail.com>
2022-12-31 19:34:28 +0100

    code: comment in navbar

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,12 +1,12 @@
 import { component$, Slot } from "@builder.io/qwik";
 import { Sidebar } from "~/components/layout";
-// import { Navbar } from "~/components/layout/navigation/navbar";
+import { Navbar } from "~/components/layout/navigation/navbar";
 
 export default component$(() => {
   return (
     <div class="p-3 h-full bg-stone-800">
       <main class="flex flex-col gap-2 md:flex-row md:justify-between h-full mt-16 sm:px-10 md:px-14 lg:px-24 xl:px-32 2xl:px-40 md:gap-12">
-        {/* <Navbar /> */}
+        <Navbar />
         <aside class="w-full self-start p-4 border rounded-lg shadow-md border-gray-700 bg-gray-800 md:sticky md:top-20 md:flex-grow-0 md:flex-shrink-0 md:basis-72 xl:top-28">
           <Sidebar />
         </aside>
