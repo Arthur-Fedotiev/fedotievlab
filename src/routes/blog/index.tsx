bf709e6188a7883057cf024939b46a79ee680cbe
arturfedotiev <artur.fedotiew@gmail.com>
2023-01-01 15:17:17 +0100

    code: implement Blog page

--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -1,73 +1,96 @@
-import { Slot, component$ } from "@builder.io/qwik";
+import { Slot, component$, Resource } from "@builder.io/qwik";
+import { getBlogPosts } from "../../modules/blog/domain/infrastructure/get-blog-posts";
+import { RequestHandler, useEndpoint } from "@builder.io/qwik-city";
+import { BlogPostPreview } from "~/modules/blog/domain/entities/blog-post.model";
 
-export default component$(() => (
-  <div class="md:py-10 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
-    <BlogPost />
-    <BlogPost />
-    <BlogPost />
-    <BlogPost />
-    <BlogPost />
-    <BlogPost />
-    <BlogPost />
-    <BlogPost />
-    <BlogPost />
-    <BlogPost />
-    <BlogPost />
-  </div>
-));
+export const onGet: RequestHandler<BlogPostPreview[]> = async () =>
+  getBlogPosts();
 
-export const Card = component$(() => {
+export default component$(() => {
+  const blogPostsResource = useEndpoint<BlogPostPreview[]>();
+
   return (
-    <a
-      href="/blog/angular-concepts"
-      tabIndex={0}
-      class="max-w-sm h-auto mx-auto rounded overflow-hidden shadow-lg"
-    >
-      <Slot name="image" />
-      <div class="px-6 py-4 ">
-        <h1 class="font-bold text-xl mb-2 hover:text-orange-500 hover:cursor-pointer">
-          <Slot name="title" />
-        </h1>
-        <p class="text-base">
-          <Slot name="description" />
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <Slot name="footer" />
-      </div>
-    </a>
+    <div class="md:py-10 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 gap-10">
+      <Resource
+        value={blogPostsResource}
+        onPending={() => <div>Loading...</div>}
+        onRejected={() => <div>Error</div>}
+        onResolved={(blogPosts) => (
+          <>
+            {blogPosts.map((blogPost) => (
+              <div
+                class="flex flex-col bg-stone-300 w-96 md:w-96 lg:w-80 xl:w-96"
+                key={blogPost.slug}
+              >
+                <BlogPost blogPostPreview={blogPost} />
+              </div>
+            ))}
+          </>
+        )}
+      />
+    </div>
   );
 });
 
-export const BlogPost = component$(() => (
-  <div class="bg-stone-300 w-96">
-    <Card>
-      <img
-        q:slot="image"
-        class="object-cover h-48 w-96"
-        src="https://images.pexels.com/photos/4220967/pexels-photo-4220967.jpeg"
-        alt="Volcano"
-      />
-      <span q:slot="title" class="text-charcoal-900 hover:text-yellow-600">
-        Volcano
-      </span>
-      <span q:slot="description" class="text-charcoal-800">
-        The picturesque lake, nestled between the DRC and Rwanda, has the
-        potential to explosively release these gases in a rare phenomenon known
-        as a limnic eruption.
-      </span>
+export interface BlogPostProps {
+  blogPostPreview: BlogPostPreview;
+}
 
-      <span q:slot="footer">
-        <span class="inline-block bg-gray-100 hover:bg-gray-200 hover:cursor-pointer rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #photography
+export const BlogPost = component$(
+  ({
+    blogPostPreview: { title, description, tags, slug, image },
+  }: BlogPostProps) => (
+    <a href={`/blog/${slug}`} tabIndex={0} class="flex flex-col flex-1">
+      <Card containerClasses={["flex-1"]}>
+        <img
+          q:slot="image"
+          class="object-cover h-48 w-96 md:w-96 lg:w-80 xl:w-96"
+          src={image}
+          alt={`Image for ${title}`}
+        />
+        <span q:slot="title" class="text-charcoal-900 hover:text-yellow-600">
+          {title}
         </span>
-        <span class="inline-block bg-gray-100 hover:bg-gray-200 hover:cursor-pointer rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #travel
+        <span q:slot="description" class="text-charcoal-800">
+          {description}
         </span>
-        <span class="inline-block bg-gray-100 hover:bg-gray-200 hover:cursor-pointer rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          #congo
-        </span>
-      </span>
-    </Card>
-  </div>
-));
+
+        {tags ? (
+          <div q:slot="footer">
+            {tags.map((tag) => (
+              <span
+                key={tag}
+                class="inline-block bg-gray-100 hover:bg-gray-200 hover:cursor-pointer rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >
+                #{tag}
+              </span>
+            ))}
+          </div>
+        ) : null}
+      </Card>
+    </a>
+  )
+);
+
+export const Card = component$(
+  ({ containerClasses = [] }: { containerClasses: string[] }) => {
+    return (
+      <section
+        class={`flex flex-col max-w-sm h-auto mx-auto rounded overflow-hidden shadow-lg ${containerClasses.join()}`}
+      >
+        <Slot name="image" />
+        <div class="px-6 py-4 ">
+          <h1 class="font-bold text-xl mb-2 hover:text-orange-500 hover:cursor-pointer">
+            <Slot name="title" />
+          </h1>
+          <p class="text-base">
+            <Slot name="description" />
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2 mt-auto">
+          <Slot name="footer" />
+        </div>
+      </section>
+    );
+  }
+);
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -7,7 +7,7 @@ export default component$(() => {
     <div class="p-3 h-full bg-stone-800">
       <main class="flex flex-col gap-2 md:flex-row md:justify-between min-h-screen mt-16 sm:px-10 md:gap-12">
         <NavBar />
-        <aside class="w-full self-start p-2 border rounded-lg shadow-md border-gray-700 bg-gray-800 md:sticky md:top-20 md:flex-grow-0 md:flex-shrink-0 md:basis-72 xl:top-28">
+        <aside class="w-full self-start p-2 border rounded-lg shadow-md border-gray-700 bg-gray-800 md:sticky md:top-28 md:flex-grow-0 md:flex-shrink-0 md:basis-72 ">
           <Sidebar />
         </aside>
 
