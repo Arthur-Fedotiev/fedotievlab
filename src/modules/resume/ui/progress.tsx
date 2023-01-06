3200c367a3e784f759582a94cb256bf2f26f9d77
arturfedotiev <artur.fedotiew@gmail.com>
2023-01-06 15:27:36 +0100

    code: animate progress bars for displaying skills

--- src/modules/resume/ui/progress.tsx
+++ src/modules/resume/ui/progress.tsx
@@ -3,15 +3,18 @@ import { component$ } from "@builder.io/qwik";
 interface ProgressProps {
   readonly label: string;
   readonly percent: number;
+  readonly ref: (el: Element) => void;
 }
 
-export const Progress = component$(({ percent, label }: ProgressProps) => (
+export const Progress = component$(({ percent, label, ref }: ProgressProps) => (
   <>
     <span class="pl-2 text-charcoal-200">{label}</span>
     <div class="shadow w-full bg-transparent rounded overflow-hidden mb-2">
       <div class="w-full h-6 bg-white rounded-full">
         <div
-          class="h-6 bg-primary-500 rounded-full"
+          ref={ref}
+          class="h-6 bg-primary-500 rounded-full transition-[width] duration-1000 ease-in-out motion-reduce:transition-none"
+          data-percent={percent}
           style={{ width: `${percent}%` }}
         ></div>
       </div>
--- src/modules/resume/ui/skills.tsx
+++ src/modules/resume/ui/skills.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useClientEffect$, $ } from "@builder.io/qwik";
 import { Progress } from "./progress";
 
 interface SkillProps {
@@ -12,27 +12,64 @@ interface SkillProps {
   }[];
 }
 
-export const Skills = component$(({ data }: SkillProps) => (
-  <section>
-    <h1 class="section-header">Skills</h1>
-    {data.map((skill) => (
-      <div key={skill.title} class="my-5">
-        <h2 class="item-header font-semibold text-lg text-center mb-2">
-          {skill.title}
-        </h2>
-        {skill.subskills.map((subskill) => (
-          <span key={subskill.name}>
-            {subskill.percent ? (
-              <Progress label={subskill.name} percent={subskill.percent} />
-            ) : null}
-            {skill.type === "tag" ? (
-              <span key={subskill.name} class="tag">
-                {subskill.name}
-              </span>
-            ) : null}
-          </span>
-        ))}
-      </div>
-    ))}
-  </section>
-));
+export const Skills = component$(({ data }: SkillProps) => {
+  const refsStore = useSignal<Element[]>([]);
+
+  useClientEffect$(() => {
+    const observer = new IntersectionObserver(
+      (entries: IntersectionObserverEntry[]) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const el = entry.target as HTMLElement;
+            const percent = entry.target.getAttribute("data-percent") ?? 0;
+            el.style.width = `${percent}%`;
+          } else {
+            const el = entry.target as HTMLElement;
+            el.style.width = "0%";
+          }
+        });
+      }
+    );
+
+    refsStore.value.forEach((el) => {
+      observer.observe(el);
+    });
+
+    return () => {
+      refsStore.value.forEach((el) => {
+        observer.unobserve(el);
+      });
+
+      observer.disconnect();
+    };
+  });
+
+  return (
+    <section>
+      <h1 class="section-header">Skills</h1>
+      {data.map((skill) => (
+        <div key={skill.title} class="my-5">
+          <h2 class="item-header font-semibold text-lg text-center mb-2">
+            {skill.title}
+          </h2>
+          {skill.subskills.map((subskill) => (
+            <span key={subskill.name}>
+              {subskill.percent ? (
+                <Progress
+                  label={subskill.name}
+                  percent={subskill.percent}
+                  ref={$((el: Element) => refsStore.value.push(el))}
+                />
+              ) : null}
+              {skill.type === "tag" ? (
+                <span key={subskill.name} class="tag">
+                  {subskill.name}
+                </span>
+              ) : null}
+            </span>
+          ))}
+        </div>
+      ))}
+    </section>
+  );
+});
