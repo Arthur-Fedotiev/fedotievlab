66de026b9e47fc225f1537d6ce04769fa23cacc3
arturfedotiev <artur.fedotiew@gmail.com>
2023-01-17 07:27:14 +0100

    bugfix: fix broken layout by removing layout for blog route

--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -9,7 +9,7 @@ export default component$(() => {
   const blogPosts = loadBlogPosts.use();
 
   return (
-    <>
+    <div class="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {blogPosts.value.map((blogPost) => (
         <div
           class="my-8 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-stone-300 duration-300 hover:-translate-y-1"
@@ -18,6 +18,6 @@ export default component$(() => {
           <Blog blogPostPreview={blogPost} />
         </div>
       ))}
-    </>
+    </div>
   );
 });
--- src/routes/blog/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { component$, Slot } from "@builder.io/qwik";
-
-export default component$(() => (
-  <div class="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-    <Slot />
-  </div>
-));
