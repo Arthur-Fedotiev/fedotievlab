a858286eb7349d65b3994ebeea9ca86267f6cdc5
arturfedotiev <artur.fedotiew@gmail.com>
2023-01-17 07:36:29 +0100

    ref: move posts to public

--- src/modules/blog/domain/infrastructure/constants.ts
+++ src/modules/blog/domain/infrastructure/constants.ts
@@ -1,3 +1,3 @@
 import { join } from "path";
 
-export const POSTS_PATH = join(process.cwd(), "_posts");
+export const POSTS_PATH = join(process.cwd(), "/public/_posts");
--- src/modules/blog/domain/infrastructure/get-post-slugs.ts
+++ src/modules/blog/domain/infrastructure/get-post-slugs.ts
@@ -1,11 +1,10 @@
 import { PostSlug } from "../entities/post-slug.model";
 import fs from "fs";
-import { join } from "path";
-const BLOG_POST_PATH = "_posts";
+import { POSTS_PATH } from "./constants";
 
 export const getPostSlugs = async (): Promise<PostSlug[]> => {
   const slugs = fs
-    .readdirSync(join(process.cwd(), BLOG_POST_PATH))
+    .readdirSync(POSTS_PATH)
     .map((path) => path.replace(/\.mdx?$/, ""))
     .map((slug) => ({ slug }));
 
