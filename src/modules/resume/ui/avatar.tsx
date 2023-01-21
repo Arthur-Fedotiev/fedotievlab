8bc0664f79ca0eec2be84129a03443847234b83e
arturfedotiev <artur.fedotiew@gmail.com>
2023-01-21 07:59:24 +0100

    code: use webp for avatar

--- src/modules/resume/ui/avatar.tsx
+++ src/modules/resume/ui/avatar.tsx
@@ -3,7 +3,7 @@ import { component$ } from "@builder.io/qwik";
 export const Avatar = component$(() => (
   <img
     class="rounded-full mx-auto w-32"
-    src="/images/avatar.jpeg"
+    src="/images/avatar.webp"
     alt="Profile Avatar"
   />
 ));
