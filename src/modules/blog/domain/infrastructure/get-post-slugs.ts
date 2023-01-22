a984e9891e879d24ce6d9fa76d88e9b5a18f15ca
arturfedotiev <artur.fedotiew@gmail.com>
2023-01-22 05:59:32 +0100

    code: use fs to retrieve post slugs

--- src/modules/blog/domain/infrastructure/get-post-slugs.ts
+++ src/modules/blog/domain/infrastructure/get-post-slugs.ts
@@ -1,17 +1,17 @@
 import { PostSlug } from "../entities/post-slug.model";
 import fs from "fs";
 import { POSTS_PATH } from "./constants";
-import db from "~/modules/shared/domain/infrastructure";
+// import db from "~/modules/shared/domain/infrastructure";
 
-const BLOG_POST_COLLECTION = "blog-posts";
+// const BLOG_POST_COLLECTION = "blog-posts";
+
+// export const getPostSlugs = async (): Promise<PostSlug[]> => {
+//   const blogPosts = await db.collection(BLOG_POST_COLLECTION).get();
+
+//   return blogPosts.docs.map((doc) => ({ slug: doc.id }));
+// };
 
 export const getPostSlugs = async (): Promise<PostSlug[]> => {
-  const blogPosts = await db.collection(BLOG_POST_COLLECTION).get();
-
-  return blogPosts.docs.map((doc) => ({ slug: doc.id }));
-};
-
-export const getPostSlugsDesired = async (): Promise<PostSlug[]> => {
   const slugs = fs
     .readdirSync(POSTS_PATH)
     .map((path) => path.replace(/\.mdx?$/, ""))
